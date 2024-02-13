f461b6d132eaf1899bb1d9c879f9b2ae97c3464c
Lam <tunglam977@gmail.com>
2024-02-13 17:40:00 +0700

    writting GetInvoice in invoiceController

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/rongdo4897/restaurant-manager-go/database"
+	"github.com/rongdo4897/restaurant-manager-go/models"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
@@ -49,7 +50,45 @@ func GetInvoices() gin.HandlerFunc {
 
 func GetInvoice() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		invoice_id := c.Param("invoice_id")
+		var invoiceModel models.Invoice
+
+		// Trả về 1 đối tượng invoice từ `invoice_id` được chỉ định, đối tượng nhận về được tham chiếu lại vào `invoiceModel`
+		err := invoiceCollection.FindOne(ctx, bson.M{"invoice_id": invoice_id}).Decode(&invoiceModel)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while fetching the invoice item"})
+			return
+		}
+
+		// Chuyển đổi dữ liệu invoice được chỉ định
+		var invoiceView InvoiceViewFormat
+
+		// Trả về danh sách item dựa trên `order_id`
+		allOrderItems, err := itemByOrder(invoiceModel.Order_id)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		invoiceView.Order_id = invoiceModel.Order_id
+		invoiceView.Payment_due_date = invoiceModel.Payment_due_date
+		// Gán lại `Payment_method`
+		invoiceView.Payment_method = "null"
+		if invoiceModel.Payment_method != nil {
+			invoiceView.Payment_method = *invoiceModel.Payment_method
+		}
+		// Gán lại các giá trị khác
+		invoiceView.Invoice_id = invoiceModel.Invoice_id
+		invoiceView.Payment_status = *&invoiceModel.Payment_status
+		invoiceView.Payment_due = allOrderItems[0]["payment_due"]
+		invoiceView.Table_number = allOrderItems[0]["table_number"]
+		invoiceView.Order_details = allOrderItems[0]["order_items"]
+
+		// Trả về kết quả
+		c.JSON(http.StatusOK, invoiceView)
 	}
 }
 
