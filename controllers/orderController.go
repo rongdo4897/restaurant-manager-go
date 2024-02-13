c0c6223ad6b4d54282d64d53b862d77d820a9c04
Lam <tunglam977@gmail.com>
2024-02-13 10:53:20 +0700

    writting GetOrders in orderController

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -1,10 +1,38 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rongdo4897/restaurant-manager-go/database"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+var orderCollection = database.OpenCollection(database.Client, "order")
 
 func GetOrders() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		// Find() được sử dụng để truy vấn tất cả các tài liệu trong bộ sưu tập.
+		// Trong trường hợp này, context.TODO() được sử dụng để tạo một ngữ cảnh mặc định (context) không có thông tin bổ sung.
+		// bson.M{} là một bộ lọc trống, chỉ đơn giản là yêu cầu tất cả các tài liệu.
+		result, err := orderCollection.Find(context.TODO(), bson.M{})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while listing order items"})
+			return
+		}
+
+		var allOrders []bson.M
+		if err = result.All(ctx, &allOrders); err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, allOrders)
 	}
 }
 
