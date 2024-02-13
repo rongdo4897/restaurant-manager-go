b2b55edda60b93ab9d788d6db1e1b7e3a93cd300
Lam <tunglam977@gmail.com>
2024-02-13 17:12:00 +0700

    writting GetInvoices in invoiceController

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -1,10 +1,49 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rongdo4897/restaurant-manager-go/database"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type InvoiceViewFormat struct {
+	Invoice_id       string
+	Payment_method   string
+	Order_id         string
+	Payment_status   *string
+	Payment_due      interface{}
+	Table_number     interface{}
+	Payment_due_date time.Time
+	Order_details    interface{}
+}
+
+var invoiceCollection = database.OpenCollection(database.Client, "invoice")
 
 func GetInvoices() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		// Find() được sử dụng để truy vấn tất cả các tài liệu trong bộ sưu tập.
+		// Trong trường hợp này, context.TODO() được sử dụng để tạo một ngữ cảnh mặc định (context) không có thông tin bổ sung.
+		// bson.M{} là một bộ lọc trống, chỉ đơn giản là yêu cầu tất cả các tài liệu.
+		result, err := invoiceCollection.Find(context.TODO(), bson.M{})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while listing invoice items"})
+			return
+		}
+
+		var allInvoices []bson.M
+		if err = result.All(ctx, &allInvoices); err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, allInvoices)
 	}
 }
 
