a1ca26b26c8c9ea82890d6cc78ab9aed9aa2b64e
Hoang Lam <tunglam977@gmail.com>
2024-02-16 16:51:28 +0700

    writting CreateTable, UpdateTable in tableController

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -10,6 +10,8 @@ import (
 	"github.com/rongdo4897/restaurant-manager-go/database"
 	"github.com/rongdo4897/restaurant-manager-go/models"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var tableCollection = database.OpenCollection(database.Client, "table")
@@ -59,12 +61,91 @@ func GetTable() gin.HandlerFunc {
 
 func CreateTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var tableModel models.Table
+		// Kiểm tra xem yêu cầu từ http có tham chiếu được tới `tableModel` không
+		if err := c.BindJSON(&tableModel); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// validate.Struct() được sử dụng để kiểm tra xem một biến cấu trúc có đáp ứng được các quy tắc kiểm tra hợp lệ (validation rules) đã được xác định hay không.
+		// Quy tắc kiểm tra này thường được định nghĩa thông qua các tag validate trong các trường của cấu trúc.
+		validationErr := validate.Struct(tableModel)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		// Set lại giá trị
+		tableModel.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		tableModel.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		tableModel.ID = primitive.NewObjectID()
+		tableModel.Table_id = tableModel.ID.Hex()
+
+		// Update lên mongo
+		result, err := tableCollection.InsertOne(ctx, tableModel)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "table item was not created - " + err.Error()})
+			return
+		}
+		defer cancel()
+
+		c.JSON(http.StatusOK, result)
 	}
 }
 
 func UpdateTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var tableModel models.Table
+		// Kiểm tra xem yêu cầu từ http có tham chiếu được tới `tableModel` không
+		if err := c.BindJSON(&tableModel); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Lấy table_id từ request
+		tableId := c.Param("table_id")
+		filter := bson.M{"table_id": tableId}
+
+		// Tạo đối tượng update
+		var updateObj primitive.D
+		// Set giá trị
+		if tableModel.Number_of_guests != nil {
+			updateObj = append(updateObj, bson.E{Key: "number_of_guests", Value: *&tableModel.Number_of_guests})
+		}
+		if tableModel.Table_number != nil {
+			updateObj = append(updateObj, bson.E{Key: "table_number", Value: *&tableModel.Table_number})
+		}
+		tableModel.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		updateObj = append(updateObj, bson.E{Key: "updated_at", Value: tableModel.Updated_at})
+
+		// Đây là một biến boolean được sử dụng để chỉ định xem truy vấn cập nhật có nên thực hiện một phép chèn mới (upsert) nếu không tìm thấy tài liệu phù hợp không.
+		// Trong trường hợp này, giá trị true cho biết rằng upsert được kích hoạt.
+		upsert := true
+		// truy vấn cập nhật sẽ thực hiện một phép upsert nếu không tìm thấy tài liệu phù hợp vì đã set = true.
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+
+		// Update lại giá trị trong mongo
+		result, err := tableCollection.UpdateOne(
+			ctx,
+			filter,
+			bson.D{{Key: "$set", Value: updateObj}},
+			&opt,
+		)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Table item update failed - " + err.Error()})
+			return
+		}
+
+		defer cancel()
+		c.JSON(http.StatusOK, result)
 	}
 }
