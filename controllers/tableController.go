e51cea6e4eebca78fabb0b894460e40e112131e1
Hoang Lam <tunglam977@gmail.com>
2024-02-16 13:26:36 +0700

    writting GetTables, GetTable in tableController

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,21 +1,59 @@
 package controllers
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/rongdo4897/restaurant-manager-go/database"
+	"github.com/rongdo4897/restaurant-manager-go/models"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 var tableCollection = database.OpenCollection(database.Client, "table")
 
 func GetTables() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		// Find() được sử dụng để truy vấn tất cả các tài liệu trong bộ sưu tập.
+		// Trong trường hợp này, context.TODO() được sử dụng để tạo một ngữ cảnh mặc định (context) không có thông tin bổ sung.
+		// bson.M{} là một bộ lọc trống, chỉ đơn giản là yêu cầu tất cả các tài liệu.
+		result, err := tableCollection.Find(context.TODO(), bson.M{})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while listing table items"})
+			return
+		}
+
+		var allTables []bson.M
+		if err = result.All(ctx, &allTables); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		c.JSON(http.StatusOK, allTables)
 	}
 }
 
 func GetTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		tableId := c.Param("table_id")
+		var tableModel models.Table
 
+		// Trả về 1 đối tượng table từ `table_id` được chỉ định, đối tượng nhận về được tham chiếu lại vào `tableModel`
+		err := tableCollection.FindOne(ctx, bson.M{"table_id": tableId}).Decode(&tableModel)
+		// Trả về lỗi nếu tồn tại
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while fetching the table item"})
+			return
+		}
+
+		c.JSON(http.StatusOK, tableModel)
 	}
 }
 
